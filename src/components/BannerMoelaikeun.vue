9bda7146dd642d744dde2af4b962697907030c82
zaidtqy <mzaidtaqy04@gmail.com>
2021-07-16 11:16:32 +0700

    API to local server

--- src/components/BannerMoelaikeun.vue
+++ src/components/BannerMoelaikeun.vue
@@ -67,7 +67,7 @@ export default {
     },
     mounted() {
         axios
-         .get("https://admin-moelaikeun.000webhostapp.com/api/products")
+         .get("http://127.0.0.1:8000/api/products")
          .then(res => (this.products = res.data.data.data))
          // eslint-disable-next-line no-console
          .catch(err => console.log(err));
