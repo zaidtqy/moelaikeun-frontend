3110e1d3d8c2b6857e167a06a57917ac351d79f9
zaidtqy <mzaidtaqy04@gmail.com>
2021-07-24 16:47:58 +0700

    Update Partner Moelaikeun

--- src/components/PartnerMoelaikeun.vue
+++ src/components/PartnerMoelaikeun.vue
@@ -2,10 +2,25 @@
     <!-- Partner Logo Section Begin -->
     <div class="partner-logo">
         <div class="container">
-            <carousel class="logo-carousel" :loop="true" :items="6" :dots="false" :nav="false" autoplay>
+            <carousel class="logo-carousel" :loop="true" :items="4" :dots="false" :nav="false" autoplay>
                 <div class="logo-item">
                     <div class="tablecell-inner">
                         <img src="img/logo-carousel/moelaikeun-1.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/moelaikeun-2.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/moelaikeun-3.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/moelaikeun-4.png" alt="" />
                     </div>
                 </div>
             </carousel>
